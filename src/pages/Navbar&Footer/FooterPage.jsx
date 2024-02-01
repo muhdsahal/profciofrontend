3fdc0df1f5174f04f9730907720f01006a24d257
sahal <sahalshalu830@gmail.com>
2024-02-01 15:42:42 +0530

    line transmission removed

--- src/pages/Navbar&Footer/FooterPage.jsx
+++ src/pages/Navbar&Footer/FooterPage.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ProfcioFooter from "../../components/Navbar&Footer/Footer";
 import { LinearProgress } from "@mui/material";
 
-function FooterPage(){
-    return(
+function FooterPage() {
+    return (
         <div>
-            <LinearProgress  className=' bg-gradient-to-r from-light-green-400 via-emerald-400 to-blue-600 ' />
+            <LinearProgress
+                className='bg-gradient-to-r from-light-green-400 via-emerald-400 to-blue-600'
+                variant="determinate"
+                value={100}  // Set the value to 100 to make it appear as a full bar
+            />            
             <ProfcioFooter />
         </div>
     )
--- src/pages/Navbar&Footer/Navbar.jsx
+++ src/pages/Navbar&Footer/Navbar.jsx
@@ -6,8 +6,11 @@ function Navbar() {
   return (
     <>
     <ResponseNavBar />
-    <LinearProgress  className=' bg-gradient-to-r from-light-green-400 via-emerald-400 to-blue-600 ' />
-
+    <LinearProgress
+  className='bg-gradient-to-r from-light-green-400 via-emerald-400 to-blue-600'
+  variant="determinate"
+  value={100}  // Set the value to 100 to make it appear as a full bar
+/>
 
     </>
   )
