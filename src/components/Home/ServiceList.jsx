0c413bd671c0ed9f7ee7dc4553adb46d18a60ba3
sahal <sahalshalu830@gmail.com>
2024-02-02 16:19:56 +0530

    service image updated

--- src/components/Home/ServiceList.jsx
+++ src/components/Home/ServiceList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Carousel,Button,h2} from "@material-tailwind/react";
-import { ServiceCatergoryURL ,ServiceListURL } from '../../constants/constants';
+import { Carousel} from "@material-tailwind/react";
+import { ServiceCatergoryURL ,ServiceListURL, base_url } from '../../constants/constants';
 
 const ServiceList = () => {
   const [services, setServices] = useState([]);
@@ -16,18 +16,20 @@ const ServiceList = () => {
       .catch(error => {
         console.error('Error fetching category options:', error);
       });
-  }, []);
-  
 
-  useEffect(() => {
-    axios.get(ServiceListURL)
+
+      axios.get(ServiceListURL)
       .then(response => {
         setServices(response.data);
       })
       .catch(error => {
         console.error('Error fetching service data:', error);
       });
+
   }, []);
+  
+
+
 
   return (
     <div style={{backgroundColor:'black'}}>
@@ -62,15 +64,21 @@ const ServiceList = () => {
   >
     {services.map((service, index) => (
       <div key={index} className="relative">
-        <img
-          src={
-            service.service_image
-              ? service.service_image
-              : 'https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80'
-          }
-          alt={`image ${index + 1}`}
+        {service.service_image ? (
+          <img
+          src={`${base_url}/${service.service_image}`}
+          alt="img_sevice"
           className="h-96 min-w-full object-cover rounded-xl border-2 border-r-emerald-600-500" // Adjust height and width as needed
-        />
+
+            />
+        ):(
+
+          <img
+          src='https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80'
+          alt="img_sevice"
+          className="h-96 min-w-full object-cover rounded-xl border-2 border-r-emerald-600-500" // Adjust height and width as needed
+          />
+          )}
         <div className="absolute top-0 left-0 w-full text-center text-blue-gray-800 p-4">
           <h2 
             className="text-5xl  font-bold animated-gradien text-transparent bg-clip-text bg-gradient-to-l from-light-green-400 via-emerald-400 to-blue-600">
